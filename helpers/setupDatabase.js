23ecfdcb064f9ba19a1629d3bd12c4af241cd7df
Deepak Pai <paidpk07@gmail.com>
2019-07-05 23:29:14 +0530

    Added db url for heroku

--- helpers/setupDatabase.js
+++ helpers/setupDatabase.js
@@ -1,15 +1,19 @@
 //require postgress node module
 const pg = require("pg");
 const dotenv = require("dotenv");
+var conString = process.env.DATABASE_URL;
+
+var db = new pg.Client(conString);
+client.connect();
 
 //setup dotenv
 dotenv.config();
-const db = new pg.Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT
-});
+// const db = new pg.Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT
+// });
 
 module.exports = db;
